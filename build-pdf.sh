f2994b5ded03cbdb0ba64132e20a14fee2adec36
Frank Hofmann <frank.hofmann@efho.de>
2021-02-03 12:11:53 +0100

    change book language to English

--- build-pdf.sh
+++ build-pdf.sh
@@ -1,2 +1,2 @@
 # a2x -f xhtml -v -a toc -a numbered -L python-introduction.adoc
-asciidoctor-pdf -a toc -a numbered -r asciidoctor-diagram solr-buch.adoc
+asciidoctor-pdf -a toc -a numbered -r asciidoctor-diagram solr-book.adoc
